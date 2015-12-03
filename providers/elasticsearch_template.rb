5c25e099b3dda171cd8fbd68abe79568d83d4629
Peter Halliday <pghalliday@gmail.com>
2015-12-03 21:19:14 +0100

    scope for File class

--- providers/elasticsearch_template.rb
+++ providers/elasticsearch_template.rb
@@ -7,6 +7,6 @@ use_inline_resources
 action :create do
   beat = new_resource.beat
   formatron_elasticsearch_template beat do
-    template JSON.parse(File.read("/etc/#{beat}/#{beat}.template.json"))
+    template JSON.parse(::File.read("/etc/#{beat}/#{beat}.template.json"))
   end
 end
